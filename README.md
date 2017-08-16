7c21c439f87b2979078764dfb7a79af9633154c0
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-08-16 21:39:24 +0100

    making changes according to the review

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 [Here you can take a look on the working project](https://lacika1981.github.io)
 
+## How to run it
+
+* Clone - simple use git clone to copy the repository to your machine and start the application with the index.html file
+
+* Download - download the zip, extract it in the desired folder and start with the index.html file
+
 I am using the zomato's API to show restaurants in London, England.
 You can click on markers or in the list on the name to get more information of each restaurant.
 
--- js/app.js
+++ js/app.js
@@ -67,7 +67,6 @@ function initMap() {
     appVM.restaurant(restaurant);
     appVM.filteredList(restaurant);
     showListings();
-    zoomNumber();
   };
 
   var failedFetch = function () {
@@ -85,287 +84,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: myLatLng,
     mapTypeControl: false,
-    styles:
-    [
-      {
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#ebe3cd"
-          }
-        ]
-      },
-      {
-        "elementType": "geometry.fill",
-        "stylers": [
-          {
-            "weight": 1
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#523735"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.stroke",
-        "stylers": [
-          {
-            "color": "#f5f1e6"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative",
-        "elementType": "geometry.stroke",
-        "stylers": [
-          {
-            "color": "#c9b2a6"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative.land_parcel",
-        "elementType": "geometry.stroke",
-        "stylers": [
-          {
-            "color": "#dcd2be"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative.land_parcel",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#ae9e90"
-          }
-        ]
-      },
-      {
-        "featureType": "landscape.natural",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#dfd2ae"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#dfd2ae"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#93817c"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.attraction",
-        "elementType": "geometry.fill",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "geometry.fill",
-        "stylers": [
-          {
-            "color": "#a5b076"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#447530"
-          }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#f5f1e6"
-          }
-        ]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#fdfcf8"
-          }
-        ]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "geometry.fill",
-        "stylers": [
-          {
-            "color": "#747678"
-          },
-          {
-            "weight": 2
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#f8c967"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [
-          {
-            "color": "#e9bc62"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway.controlled_access",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#e98d58"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway.controlled_access",
-        "elementType": "geometry.stroke",
-        "stylers": [
-          {
-            "color": "#db8555"
-          }
-        ]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "geometry.fill",
-        "stylers": [
-          {
-            "color": "#747678"
-          },
-          {
-            "weight": 4.5
-          }
-        ]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "geometry.stroke",
-        "stylers": [
-          {
-            "weight": 6.5
-          }
-        ]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "labels.text",
-        "stylers": [
-          {
-            "color": "#747678"
-          }
-        ]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#feee0a"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#dfd2ae"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.line",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#8f7d77"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.line",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-          {
-            "color": "#ebe3cd"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.station",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#dfd2ae"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [
-          {
-            "color": "#24e2e5"
-          },
-          {
-            "saturation": -45
-          },
-          {
-            "lightness": -40
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#92998d"
-          }
-        ]
-      }
-    ]
+    styles: style
   });
   getRestaurants(); // calls the getRestaurants method
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(search);
@@ -375,18 +94,6 @@ function initMap() {
     animation: google.maps.Animation.DROP
   });
 }
-
-// checking the screen width to determine the zoom number
-var zoomNumber = function () {
-  if (window.innerWidth > 700) {
-    zm = 17;
-  }
-  if (window.innerWidth > 600) {
-    zm = 16;
-  } else {
-    zm = 15;
-  }
-};
 
 var populateInfoWindow = function (marker, infoWindow) {
   // Check to make sure the infowindow is not already opened on this marker.
@@ -414,7 +121,9 @@ var showListings = function () {
     bounds.extend(markers[i].position);
   }
   map.fitBounds(bounds);
-  map.setZoom(zm);
+  google.maps.event.addDomListener(window, 'resize', function () {
+    map.fitBounds(bounds); // `bounds` is a `LatLngBounds` object
+  });
 };
 
 // stop bouncing the marker
@@ -436,6 +145,7 @@ function AppViewModel() {
   self.filteredList = ko.observableArray(); // array with the restaurants
   self.locations = ko.observableArray(locations); // holds the lat and lng of each restaurant
   self.filterItemLength = ko.observable(); // holds the value of the length of the restaurantArray
+  self.visibleExample = ko.observable(false);
 
   // called by the reset to set everything to the initial state
   self.clear = function () {
@@ -443,6 +153,11 @@ function AppViewModel() {
     infoWindow.close(map);
     self.filterItem('');
     self.filteredList(self.restaurantArrayFix()); // updates the filteredList calling the restaurantArrayFix array
+  };
+
+  self.clickMenu = function () {
+    self.visibleExample(!self.visibleExample());
+    console.log('test');
   };
 
   //custom binding to fadeIn and fadeOut the reset button - from learn.knockoutjs.com
@@ -464,42 +179,43 @@ function AppViewModel() {
     var restaurantArray = self.restaurant();
     self.filterItemLength(restaurantArray.length);
     if (!filter) {
-      return markers;
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+      }
+      self.filteredList(self.restaurantArrayFix());
     } else {
       self.filteredList([]); // empty the array before pushing the restaurants what contains the typed characters
       var bounds = new google.maps.LatLngBounds();
-      for (var i = 0, len = self.filterItemLength(); i < len; i++) {
-        if (restaurantArray[i].names.toLowerCase().indexOf(filter) != -1) {
-          restaurantArray[i].markers.setMap(map);
-          self.filteredList.push(restaurantArray[i]); // adding the restaurant array to update the menu with the available restaurants
-          bounds.extend(restaurantArray[i].locations);
+      for (var j = 0, len = self.filterItemLength(); j < len; j++) {
+        if (restaurantArray[j].names.toLowerCase().indexOf(filter) != -1) {
+          restaurantArray[j].markers.setMap(map);
+          self.filteredList.push(restaurantArray[j]); // adding the restaurant array to update the menu with the available restaurants
+          bounds.extend(restaurantArray[j].locations);
           map.fitBounds(bounds);
-          map.setZoom(zm);
+          map.setZoom(17);
         } else {
-          restaurantArray[i].markers.setMap(null);
+          restaurantArray[j].markers.setMap(null);
         }
       }
     }
   });
 
-
-  // shows the restaurants on the map
   self.showRestaurant = function (restaurant) {
     var clickedRestaurant = restaurant.addresses;
-    for (var key in self.restaurant()) {
-      if (clickedRestaurant === self.restaurant()[key].addresses) {
-        self.restaurant()[key].markers.setAnimation(google.maps.Animation.BOUNCE);
-        stopAnimation(self.restaurant()[key].markers); // calling the stopAnimation function to stop the marker bouncing
-        map.panTo(self.restaurant()[key].markers.position); // centering the markers on the map
-        map.setZoom(zm + 2);
-        infoWindow.setContent('<div><b>Restaurant name:</b> ' + self.restaurant()[key].names + '</div><div><b>Address:</b> ' + self.restaurant()[key].addresses + '</div><div><b>Cuisine:</b><br>' + self.restaurant()[key].cuisines + '</div><div><b>Price range: </b>' + self.restaurant()[key].prices + '</div>');
-        infoWindow.open(map, self.restaurant()[key].markers);
+    self.restaurant().forEach(function (restaurant) {
+      if (clickedRestaurant === restaurant.addresses) {
+        restaurant.markers.setAnimation(google.maps.Animation.BOUNCE);
+        stopAnimation(restaurant.markers); // calling the stopAnimation function to stop the marker bouncing
+        map.panTo(restaurant.markers.position); // centering the markers on the map
+        map.setZoom(19);
+        infoWindow.setContent('<div><b>Restaurant name:</b> ' + restaurant.names + '</div><div><b>Address:</b> ' + restaurant.addresses + '</div><div><b>Cuisine:</b><br>' + restaurant.cuisines + '</div><div><b>Price range: </b>' + restaurant.prices + '</div>');
+        infoWindow.open(map, restaurant.markers);
       }
-    }
+    });
   };
 }
 
-// menu control
+/* // menu control
 var moved = false;
 
 $(function () {
@@ -515,7 +231,7 @@ $(function () {
       moved = false;
     }
   });
-});
+}); */
 
 appVM = new AppViewModel();
 
